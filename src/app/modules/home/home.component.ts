2d1448a8612ea8d6bfc20a15a18086209d47b54d
vsje <davidfelipehernandez1@gmail.com>
2022-12-10 18:31:54 -0500

    home page galery carousel

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Images } from 'src/app/data';
+
+import SwiperCore, {
+  SwiperOptions,
+  Autoplay,
+  Pagination,
+  Navigation,
+} from 'swiper';
+
+SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
+  images: string[] = Images;
 
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    keyboard: true,
+    centeredSlides: true,
+    navigation: true,
+    autoplay: {
+      delay: 2200,
+      disableOnInteraction: true,
+    },
+    // pagination: { clickable: true },
+    scrollbar: { draggable: true },
+    breakpoints: {
+      800: {
+        slidesPerView: 3,
+      },
+      600: {
+        slidesPerView: 2,
+      },
+    },
+  };
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, SwiperModule],
 })
 export class HomeModule {}
