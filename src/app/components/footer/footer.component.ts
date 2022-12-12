30636cda7f3615b0d38396015d79b1479908ac31
vsje <davidfelipehernandez1@gmail.com>
2022-12-11 22:22:47 -0500

    standalone footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxBootstrapIconsModule],
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
