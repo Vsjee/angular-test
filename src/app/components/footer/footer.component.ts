f0983d1ae260f3f4d37ad2d10dfc44fa511f057c
vsje <davidfelipehernandez1@gmail.com>
2022-12-12 14:51:17 -0500

    form handle

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NgxBootstrapIconsModule],
+  imports: [CommonModule, NgxBootstrapIconsModule, ReactiveFormsModule],
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  userEmail = {};
+
+  checkoutForm = this.formBuilder.group({
+    email: '',
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit(): void {
+    this.userEmail = this.checkoutForm.value; //save in userEmail
+    console.warn('Your email has been submitted', this.checkoutForm.value);
+    console.log(this.userEmail);
+    this.checkoutForm.reset();
+  }
+}
