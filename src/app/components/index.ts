ac6b4aaee1a67bf164c0f2e2a81318b0154225ab
david hernandez <davidfelipehernandez1@gmail.com>
2022-12-14 21:45:16 -0500

    toolbar responsive

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,4 +1,3 @@
 export * from './coffee-card';
 export * from './footer';
-export * from './index';
 export * from './toolbar';
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   standalone: true,
-  imports: [AppRoutingModule, MatToolbarModule, MatIconModule],
+  imports: [
+    AppRoutingModule,
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  menu: boolean = false;
+  menuHidde: boolean = true;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    if (window.screen.width < 510) {
+      this.menu = true;
+      this.menuHidde = false;
+    } else {
+      this.menu = false;
+      this.menuHidde = true;
+    }
+  }
+}
