1dbe1302b1645e62b2b9a557c174cd56b233e9aa
david hernandez <davidfelipehernandez1@gmail.com>
2022-12-21 20:45:47 -0500

    login module

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,3 +1,5 @@
+export * from './favorites';
 export * from './home';
 export * from './hot-coffee';
 export * from './iced-coffee';
+export * from './login';
---
+++ src/app/modules/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  name = new FormControl();
+  password = new FormControl();
+
+  handleSubmit() {
+    if (this.name.value !== null && this.password.value !== null) {
+      this.auth.authenticate(this.name.value, this.password.value);
+    }
+    this.router.navigate(['./favorites']);
+  }
+}
