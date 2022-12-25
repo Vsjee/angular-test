63148f42f519e7a554bad96f73d7a74a354eb27c
david hernandez <davidfelipehernandez1@gmail.com>
2022-12-25 18:07:38 -0500

    print favoritList from store

--- src/app/modules/favorites/favorites.component.ts
+++ src/app/modules/favorites/favorites.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
 import { FavoritesInfo } from 'src/app/core';
 import { AppState, selectFavoriteList } from 'src/app/state';
 
@@ -12,13 +11,11 @@ import { AppState, selectFavoriteList } from 'src/app/state';
 export class FavoritesComponent {
   constructor(private store: Store<AppState>) {}
 
-  saveFavorites$: Observable<FavoritesInfo[]> = new Observable();
   favoriteList: FavoritesInfo[] = [];
 
   ngOnInit() {
-    this.saveFavorites$ = this.store.select(selectFavoriteList);
     this.store
       .select(selectFavoriteList)
-      .subscribe((state) => (this.favoriteList = state));
+      .subscribe((state: FavoritesInfo[]) => (this.favoriteList = state));
   }
 }
