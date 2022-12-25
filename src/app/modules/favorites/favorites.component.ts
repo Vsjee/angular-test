2837c3c62841055ff960661eb4b1184f4aaadc9b
david hernandez <davidfelipehernandez1@gmail.com>
2022-12-25 14:09:33 -0500

    favorites page impl card comp

--- src/app/modules/favorites/favorites.component.ts
+++ src/app/modules/favorites/favorites.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { FavoritesInfo } from 'src/app/core';
+import { AppState, selectFavoriteList } from 'src/app/state';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.scss'],
 })
-export class FavoritesComponent {}
+export class FavoritesComponent {
+  constructor(private store: Store<AppState>) {}
+
+  saveFavorites$: Observable<FavoritesInfo[]> = new Observable();
+  favoriteList: FavoritesInfo[] = [];
+
+  ngOnInit() {
+    this.saveFavorites$ = this.store.select(selectFavoriteList);
+    this.store
+      .select(selectFavoriteList)
+      .subscribe((state) => (this.favoriteList = state));
+  }
+}
--- src/app/modules/favorites/favorites.module.ts
+++ src/app/modules/favorites/favorites.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoritesComponent } from './favorites.component';
 import { FavoritesRoutingModule } from './favorites-routing.module';
+import { CoffeeCardComponent } from 'src/app/components';
 
 @NgModule({
   declarations: [FavoritesComponent],
-  imports: [CommonModule, FavoritesRoutingModule],
+  imports: [CommonModule, FavoritesRoutingModule, CoffeeCardComponent],
 })
 export class FavoritesModule {}
